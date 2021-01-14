77c48372c580763cd0889cbe9fb9061781d9fd46
dream-ct <75789699+dream-ct@users.noreply.github.com>
2021-01-14 21:05:46 +0800

    deque大小操作
    deque.empty();                    //判断容器是否为空
    deque.size();                     //返回容器中元素的个数
    deque.resize(num);                //重新指定容器的长度为num,若容器变长，则以默认值填充新位置。
    ​			                      //如果容器变短，则末尾超出容器长度的元素被删除。
    deque.resize(num, elem);          //重新指定容器的长度为num,若容器变长，则以elem值填充新位置。
                                     //如果容器变短，则末尾超出容器长度的元素被删除。

---
+++ c++.deque.c
@@ -0,0 +1,50 @@
+//3.3.4 deque大小操作
+//功能描述：
+//对deque容器的大小进行操作
+//函数原型：
+//deque.empty();                    //判断容器是否为空
+//deque.size();                     //返回容器中元素的个数
+//deque.resize(num);                //重新指定容器的长度为num,若容器变长，则以默认值填充新位置。
+//​			                      //如果容器变短，则末尾超出容器长度的元素被删除。
+//deque.resize(num, elem);          //重新指定容器的长度为num,若容器变长，则以elem值填充新位置。
+//​                                  //如果容器变短，则末尾超出容器长度的元素被删除。​
+#include<iostream>
+using namespace std;
+#include<deque>
+void printDeque(const deque<int>&d){
+	for (deque<int>::const_iterator it = d.begin(); it != d.end(); it++){
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+void test01(){
+	deque<int>d1;
+	for (int i = 0; i < 10; i++){
+		d1.push_back(i);
+	}
+	printDeque(d1);
+	if (d1.empty()){
+		cout << "d1为空" << endl;
+	}
+	else{
+		cout << "d1不为空" << endl;
+		cout << "d1的大小为："<< d1.size() << endl;
+	}
+	//deque容器没有容量概念
+	//d1.resize(15);
+	//printDeque(d1);
+	d1.resize(15, 1);
+	printDeque(d1);
+	d1.resize(5);
+	printDeque(d1);
+}
+int main(){
+	test01();
+	system("pause");
+	return 0;
+}
+//总结：
+//* deque没有容量的概念
+//* 判断是否为空-- - empty
+//* 返回元素个数-- - size
+//* 重新指定个数-- - resize
